4316506eb891022aa619b5b4b6137d99ad1b9060
Haidar Ali <haidar47x@gmail.com>
2025-09-25 04:10:43 +0500

    declutter: removed unused files and trimmed down gateway module

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { GatewayController } from './gateway.controller';
-import { GatewayService } from './gateway.service';
 import { CoreModule } from '@lib/core';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [CoreModule, AuthModule],
-  controllers: [GatewayController],
-  providers: [GatewayService],
 })
 export class GatewayModule {}
