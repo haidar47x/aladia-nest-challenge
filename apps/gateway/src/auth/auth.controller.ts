21ce3ef46062485029ed87856ec87db755a5aec2
Haidar Ali <haidar47x@gmail.com>
2025-09-25 04:08:28 +0500

    progress: added endpoint for user login

--- apps/gateway/src/auth/auth.controller.ts
+++ apps/gateway/src/auth/auth.controller.ts
@@ -1,14 +1,30 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { NetworkingService } from '@lib/core';
-import { RegisterUserDto, UserRto } from '@lib/common';
+import { LoginUserDto, RegisterUserDto, UserRto } from '@lib/common';
 import { Observable } from 'rxjs';
 
 @Controller('auth')
+@UsePipes(new ValidationPipe({ transform: true }))
 export class AuthController {
   constructor(private readonly networkingService: NetworkingService) {}
 
+  @Post('login')
+  loginUser(@Body() data: LoginUserDto): Observable<any> {
+    return this.networkingService.authClient.send<any>(
+      { cmd: 'login-user' },
+      data,
+    );
+  }
+
   @Post('register')
-  registerUser(@Body() data: RegisterUserDto) {
+  registerUser(@Body() data: RegisterUserDto): Observable<UserRto> {
     return this.networkingService.authClient.send<UserRto>(
       {
         cmd: 'register-user',
