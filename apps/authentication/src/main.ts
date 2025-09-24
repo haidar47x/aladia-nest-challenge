725f5b959264be150acb2902e6d084393792c8f7
Haidar Ali <haidar47x@gmail.com>
2025-09-25 00:12:29 +0500

    progress: basic microservice setup for auth module

--- apps/authentication/src/main.ts
+++ apps/authentication/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AuthenticationModule } from './authentication.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthenticationModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthenticationModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 3001
+      }
+    },
+  );
+  await app.listen();
 }
+
 bootstrap();
