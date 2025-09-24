ffe5149438765349c5125976debb1c528fa379cb
Haidar Ali <haidar47x@gmail.com>
2025-09-25 04:09:52 +0500

    progress: added Users schema and relevant DB deps

--- apps/authentication/src/authentication.module.ts
+++ apps/authentication/src/authentication.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 'mongodb://localhost:27017',
+    ),
+    UsersModule,
+  ],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
 })
