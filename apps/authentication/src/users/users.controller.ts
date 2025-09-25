c9434781c643fa31bc65f71924f0ad26ccd9a8a5
haidar47x <haidar47x@gmail.com>
2025-09-25 06:20:50 +0500

    progress: added users controller

---
+++ apps/authentication/src/users/users.controller.ts
@@ -0,0 +1,24 @@
+import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { MessagePattern } from '@nestjs/microservices';
+import { RegisterUserDto } from '@lib/common';
+
+@Controller()
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @MessagePattern({ cmd: 'register-user' })
+  async registerUser(dto: RegisterUserDto) {
+    return this.usersService.register(dto);
+  }
+
+  @MessagePattern({ cmd: 'login-user' })
+  async loginUser() {
+    /** To be implemented */
+  }
+
+  @MessagePattern({ cmd: 'get-users' })
+  async getAllUsers() {
+    return this.usersService.findAll();
+  }
+}
